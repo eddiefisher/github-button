a50a3bc8c77b804c7d8ba502ff523904390e1c86
eddiefisher <eddifisher@bk.ru>
2013-03-30 13:45:03 +0400

    first commit

---
+++ lib/github/button.rb
@@ -0,0 +1,7 @@
+require "github/button/version"
+
+module Github
+  module Button
+    # Your code goes here...
+  end
+end
---
+++ lib/github/button/version.rb
@@ -0,0 +1,5 @@
+module Github
+  module Button
+    VERSION = "0.0.1"
+  end
+end
